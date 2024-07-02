cf78abb1251e2e080a677831499b3c8668a97fe5
Vlados <vladik00848@gmail.com>
2024-07-02 18:58:05 +0500

    1

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ app.use(cors());
 // Routes
 app.use("/api", aggregatorRoutes(aggregatorController));
 
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 5031;
 app.listen(port, () => console.log(`Сервер запущен на порту ${port}`));
