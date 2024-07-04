58e892166271eb4b7f9a334f9edef639486abeef
Владислав <vladik00848@gmail.com>
2024-07-04 15:46:30 +0500

    добавил функцию для получения города по ip

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ app.use(cors());
 // Routes
 app.use("/api", aggregatorRoutes(aggregatorController));
 
-const port = process.env.PORT || 5030;
+const port = process.env.PORT || 5036;
 app.listen(port, () => console.log(`Сервер запущен на порту ${port}`));
--- controllers/aggregatorController.js
+++ controllers/aggregatorController.js
@@ -13,6 +13,7 @@ class AggregatorController {
       8: { id: 8, name: "Дом.ру", img: "domru.svg" },
       9: { id: 9, name: "Сибирский медведь", img: "sibMedved.svg" },
     };
+    this.apiKeyDadata = "bbbdb08051ba3df93014d80a721660db6c19f0db";
   }
 
   getTariffsByDistrictId = async (req, res) => {
@@ -465,6 +466,56 @@ class AggregatorController {
       res.status(200).json({ tariff });
     } catch (error) {
       res.status(500).json({ error: error.message });
+    }
+  };
+
+  getIpAndCity = async (req, res) => {
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log(ip);
+    try {
+      // Запрос к DaData для получения информации о местоположении по IP
+      const dadataResponse = await fetch(
+        "http://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Token ${this.apiKeyDadata}`,
+          },
+          body: JSON.stringify({ ip: ip }),
+        }
+      );
+
+      if (!dadataResponse.ok) {
+        throw new Error(`DaData API error: ${dadataResponse.statusText}`);
+      }
+
+      const dadataData = await dadataResponse.json();
+      console.log(dadataData);
+      // Проверяем, получили ли мы корректные данные
+      if (!dadataData || !dadataData.location || !dadataData.location.data) {
+        throw new Error("Invalid data received from DaData");
+      }
+
+      const cityKladrId = dadataData.location.data.city_kladr_id;
+
+      const cityData = await aggregatorService.getCityName(cityKladrId);
+      const cityName = cityData["engname"];
+
+      // Проверяем, получили ли мы корректные данные
+      if (!cityData || !cityName) {
+        throw new Error("Invalid data received from local API");
+      }
+
+      // Отправляем имя города в ответ
+      res.status(200).json({ city: cityName });
+    } catch (error) {
+      console.error("Error:", error.message);
+      res.status(500).json({
+        error: "Failed to fetch data",
+        details: error.message,
+      });
     }
   };
 }
